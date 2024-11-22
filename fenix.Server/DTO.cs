b0aa32105f5317e51166eb2198d5b928237172ff
bar <cohen.bau@gmail.com>
2024-11-22 12:39:49 +0200

    new dto

---
+++ fenix.Server/DTO.cs
@@ -0,0 +1,49 @@
+﻿namespace fenix.Server
+{
+    public class DTO
+    {
+        public class ServerResponse<T> {
+            public required bool success { get; set; }
+            public ErrorMessage[] errors { get; set;}
+            public required Guid hash { get; set; }
+            public required T data { get; set; }
+
+        }
+        public class ErrorMessage
+        {
+            public int Code { get; set; }
+            public required string Message { get; set; }
+            public required string Description { get; set; }
+        }
+  
+        public class Login
+        {
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+        }
+        public class LoginResponse
+        {
+            
+            public required string Token { get; set; }
+        }
+        public class GitHubResponse
+        {
+            public int Total_count { get; set; }
+            public bool IncompleteResults { get; set; }
+            public List<Item> Items { get; set; }
+
+            public class Item
+            {
+                public long Id { get; set; }
+                public string Full_name { get; set; }
+                public Owner Owner { get; set; }
+
+            }
+            public class Owner
+            {
+                public string Avatar_url { get; set; }
+            }
+        }
+
+    }
+}
